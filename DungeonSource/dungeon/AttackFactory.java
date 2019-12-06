76b90c209adc41529e44952f95a8b62dae5df350
Robin <rdeskins17@gmail.com>
2019-12-05 19:14:36 -0800

    Added HealPotionsFound, VisionPotionsFound, PillarsFound
    Added new data fields and getters

--- DungeonSource/dungeon/AttackFactory.java
+++ DungeonSource/dungeon/AttackFactory.java
@@ -12,10 +12,23 @@ public class AttackFactory {
 		pool.add(new ThiefSurpriseAttackBehavior());
 		pool.add(new WarriorAttackBehavior());
 		pool.add(new WarriorCrushingBlowBehavior());
+		pool.add(new MonkAttackBehavior());
+		pool.add(new MonkPunchAttackBehavior());
+		pool.add(new DinosaurAttackBehavior());
+		pool.add(new DinosaurStompAttackBehavior());
+		pool.add(new MockAttackBehavior());
 	}
 	
 	public AttackBehavior getAttack(String name) {
 		switch(name) {
+		case "Dinosaur Stomp Attack":
+			return pool.get(9);
+		case "Dinosaur Attack":
+			return pool.get(8);
+		case "Monk Punch Attack":
+			return pool.get(7);
+		case "Monk Attack":
+			return pool.get(6);
 		case "Warrior Crushing Blow":
 			return pool.get(5);
 		case "Warrior Attack":
@@ -29,7 +42,7 @@ public class AttackFactory {
 		case "Base Attack":
 			return pool.get(0);
 		default:
-			throw new IllegalArgumentException("Invalid string name in AttackFactory getAttack()");
+			return pool.get(10); 
 		}
 		
 	}
