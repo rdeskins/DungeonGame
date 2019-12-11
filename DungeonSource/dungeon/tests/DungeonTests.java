f31e60c85241d095129f151a45a490787b3ed74c
Robin <rdeskins17@gmail.com>
2019-12-11 13:25:04 -0800

    Created DungeonTests

--- DungeonSource/dungeon/tests/DungeonTests.java
+++ DungeonSource/dungeon/tests/DungeonTests.java
@@ -2,36 +2,76 @@ package dungeon.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
 import dungeon.Dungeon;
 import dungeon.Memento;
 import dungeon.Room;
 import dungeon.Warrior;
+import dungeon.Pillar;
 
 class DungeonTests {
 
 	@Test
 	void testCreateDungeonSetsFields() {
 		Dungeon dungeon = new Dungeon();
-		
+		dungeon.createDungeon();
+		Room[][] rooms = dungeon.getDungeonRooms();
 
+		assertTrue(rooms.length == 5);
+		assertTrue(rooms[0].length == 5);
 	}
 
 	@Test
 	void testSetUpDungeon() {
 		Dungeon dungeon = new Dungeon();
-		
+		dungeon.createDungeon();
 		dungeon.setUpDungeon(new Warrior("Testy boi"));
 		
-		System.out.println(dungeon);
+		Room[][] rooms = dungeon.getDungeonRooms();
+		int totalPillars = 0;
+		int totalEntrance = 0;
+		int totalExit = 0;
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+				Room currentRoom = rooms[x][y];
+				if (currentRoom.getItem() instanceof Pillar)
+					totalPillars++;
+				else if (currentRoom.isEntrance())
+					totalEntrance++;
+				else if (currentRoom.isExit())
+					totalExit++;
+			}
+		}
+		
+		assertTrue(totalPillars == 4);
+		assertTrue(totalEntrance == 1);
+		assertTrue(totalExit == 1);
 	}
 
 	@Test
 	void testToString() {
 		Dungeon dungeon = new Dungeon();
+		dungeon.createDungeon();
 		
-		assertNotNull(dungeon.toString());
+		assertEquals(dungeon.toString(),
+				 "* * ** * ** * ** * ** * *\n"
+				+"* E || E || E || E || E *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* E || E || E || E || E *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* E || E || E || E || E *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* E || E || E || E || E *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* - ** - ** - ** - ** - *\n"
+				+"* E || E || E || E || E *\n"
+				+"* * ** * ** * ** * ** * *\n");
+		//No further tests because of RNG of item placement.
 	}
 
 	@Test
